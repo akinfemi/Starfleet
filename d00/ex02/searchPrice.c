91fa29ea3c10c3fbfc13f8661a3dd3bca1a9fa9e
akinfemi <akinfemi.aluko@gmail.com>
2017-10-08 17:31:16 -0700

    binary search

--- d00/ex02/searchPrice.c
+++ d00/ex02/searchPrice.c
@@ -7,19 +7,10 @@ int searchPrice(struct s_art **arts, int n, char *name)
     struct s_art **art_half = NULL;
     if (n <= 0)
         return (-1);
-    if (strcmp((*arts)->name, name) == 0)
-        return ((*arts)->price);
     art_half = arts + n/2;
-    if (strcmp(name, (*art_half)->name) == 0)
-        return (*art_half)->price;
-    if (strcmp(name, (*art_half)->name) > 0){
-        printf ("here:   %s----%d\n",(*art_half)->name, n);
-        return (searchPrice(art_half, n - n/2, name));
-    }
+    if (strcmp(name, (*art_half)->name) > 0)
+        return (searchPrice(art_half, n/2 + 1, name));
     else if (strcmp(name, (*art_half)->name) < 0)
-    {
-        printf ("here:   %s----%d\n",(*art_half)->name, n);
-        return (searchPrice(arts, n/2, name));
-    }
-    return (-1);
+        return (searchPrice(arts, n/2 - 1, name));
+    return (*art_half)->price;
 }
